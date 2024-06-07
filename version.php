4855d8a6b945978dca80192069749d5da0c0e2c1
FDC Wenman <139725768+FDCWenman@users.noreply.github.com>
2024-06-07 15:58:57 +0800

    Create version.php

---
+++ version.php
@@ -0,0 +1,11 @@
+<?php
+# --- CSS & JS VERSION CONTROL ---
+
+// admin
+$config['admin.version'] = '2.12.9';
+
+// teacher
+$config['teacher.version'] = '2.11.87';
+
+// user
+$config['user.version'] = '9.14.58';
